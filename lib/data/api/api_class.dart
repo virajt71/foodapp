129a650fcc52bc659f02c7674f100b1a315f6997
virajt71 <soulr27@gmail.com>
2022-06-27 06:41:58 +0200

    v4

--- lib/data/api/api_class.dart
+++ lib/data/api/api_class.dart
@@ -5,9 +5,13 @@ class ApiClient extends GetConnect implements GetxService {
   final String appBaseUrl;
 
   late Map<String, String> _mainHeaders;
-  
+
   ApiClient({required this.appBaseUrl}) {
     baseUrl = appBaseUrl;
     timeout = Duration(seconds: 30);
+    _mainHeaders = {
+      'Content-type': 'application/json; charset=UTF-8',
+      'Authorization': 'bearer $token'
+    };
   }
 }
