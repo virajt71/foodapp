7e0e688bfa6d982f64cdaf91d550203fff4540ca
virajt71 <soulr27@gmail.com>
2022-06-27 06:30:46 +0200

    v4

---
+++ lib/data/api/api_class.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class ApiClient extends GetConnect implements GetxService {
+  late String token;
+  final String appBaseUrl;
+
+  late Map<String, String> _mainHeaders;
+  
+  ApiClient({required this.appBaseUrl}) {
+    baseUrl = appBaseUrl;
+    timeout = Duration(seconds: 30);
+  }
+}
